38f342cce99eb3492aae11533a617ea97e891ded
angelachen <angelachen085@gmail.com>
2020-08-25 21:41:18 -0400

    hello world p2

---
+++ learn_go/hello/hello_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t *testing.T, got string, want string) {
+		t.Helper() // tells the test suite that this method is a helper (when it fails, the line # reported is function call rather than test helper)
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+	
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Angela")
+		want := "Hello, Angela"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+	
+}
